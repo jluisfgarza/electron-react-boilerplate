edc602aa63033b7e0788b61a5d3a41d3c7a6e278
SNx42 <seannowotny@live.de>
2019-12-31 17:11:51 +1000

    Covert app component to functional component

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+export const App = () =>
+{
+  return (
+    <div className="App">
+      <h1>Test</h1>
+    </div>
+  );
+}
+
+export default App;
